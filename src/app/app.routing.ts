54f07f24c2feece0c86024a3719050cf00edbc1f
Pablo Cifuentes <pablo11cifuentes@gmail.com>
2019-01-21 09:39:28 -0600

    Formularios con validacion

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,22 +1,22 @@
 import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 // Importar componentes
 import { EmpleadoComponent } from './empleado/empleado.component';
-import { FrutaComponent }    from './fruta/fruta.component';
-import { HomeComponent }     from './home/home.component';
-import { ContactoComponent}  from './contacto/contacto.component';
-import {CochesComponent} from './coches/coches.component';
+import { FrutaComponent } from './fruta/fruta.component';
+import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { CochesComponent } from './coches/coches.component';
 
 const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path:'empleado', component:  EmpleadoComponent},
-  {path:'fruta', component:  FrutaComponent},
-  {path:'home', component:  HomeComponent},
-  {path:'contacto', component:  ContactoComponent},
-  {path:'contacto/:page', component:  ContactoComponent},
-  {path:'coches', component:  CochesComponent},
-  {path: '**', component: HomeComponent}
+  { path: '', component: HomeComponent },
+  { path: 'empleado', component: EmpleadoComponent },
+  { path: 'fruta', component: FrutaComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'contacto/:page', component: ContactoComponent },
+  { path: 'coches', component: CochesComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 export const appRoutingProviders: any[] = [];
